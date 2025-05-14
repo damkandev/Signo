aecf928e0fe789059212ae82437abb400f9be109
Damián Panes <damian@granmenu.me>
2025-05-14 11:51:54 -0400

    Refactor server.py for improved readability and maintainability

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from mcp.server.fastmcp import FastMCP
 import psycopg2
 import os
-from jinja2 import Template
-import subprocess
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+import pdfkit
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 mcp = FastMCP("Signo")
 
@@ -46,5 +50,42 @@ def listar_tablas() -> str:
         return f"Error al ejecutar la consulta: {str(e)}"
 
 @mcp.tool()
-def crear_contrato() -> str:
-    pass
+def crear_contrato(nombre_empleado: str, dni_empleado: str, objeto_contrato:str, empleado_pago:int, nombre_proyecto: str, nombre_empresa: str, horas_semanales:int, fecha_inicio:str, fecha_fin:str) -> str:
+    """Genera un contrato en formato PDF con la información del empleado y el proyecto"""
+    try:
+        template_dir = os.path.join(os.path.dirname(__file__), "templates")
+        template_name = "plantilla.html"
+        output_dir = r"C:\contratos"
+
+        os.makedirs(output_dir, exist_ok=True)
+        env = Environment(loader=FileSystemLoader(template_dir))
+        template = env.get_template(template_name)
+
+        contexto = {
+            'nombre_empleado': nombre_empleado,
+            'fecha_inicio': fecha_inicio,
+            'fecha_fin': fecha_fin,
+            'horas_semanales': horas_semanales,
+            'nombre_empresa': nombre_empresa,
+            'dni_empleado': dni_empleado,
+            'objeto_contrato': objeto_contrato,
+            'empleado_pago': empleado_pago,
+            'nombre_proyecto': nombre_proyecto
+        }
+
+        html_renderizado = template.render(contexto)
+
+        html_temp_path = os.path.join(output_dir, "temp.html")
+        with open(html_temp_path, "w", encoding="utf-8") as f:
+            f.write(html_renderizado)
+
+        filename = f"contrato_{nombre_empleado.replace(' ', '_')}_{datetime.now().strftime('%Y%m%d%H%M%S')}.pdf"
+        output_pdf_path = os.path.join(output_dir, filename)
+
+        config = pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
+        pdfkit.from_file(html_temp_path, output_pdf_path, configuration=config)
+
+        return output_pdf_path
+
+    except Exception as e:
+        return f"Error al generar el contrato: {str(e)}"
